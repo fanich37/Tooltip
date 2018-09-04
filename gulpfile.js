93f6d9d487891b264ab8d7c8c9cd0e8ed4bd5f7a
fanich37 <icima.fan@gmail.com>
2018-09-04 08:06:07 +0300

    update json

--- gulpfile.js
+++ gulpfile.js
@@ -12,57 +12,47 @@ const bs = require('browser-sync').create();
 const isDebug = process.env.NODE_ENV !== 'production';
 const { PORT, OPEN, NODE_ENV, TUNNEL } = process.env;
 
-gulp.task('default', () => 
-	runSequence(
-		'copy',
-		'scripts',
-		'server',
-		'watch'
-	)
-);
+gulp.task('default', () => runSequence('copy', 'scripts', 'server', 'watch'));
 
 gulp.task('scripts', () => {
-	gulp.src('src/*.js')
-		.pipe(plumber())
-		.pipe(babel())
-		.pipe(rename({suffix: '.min'}))
-		.pipe(gulpIf(!isDebug, uglify()))
-		.pipe(gulp.dest('dist'));
+  gulp
+    .src('src/*.js')
+    .pipe(plumber())
+    .pipe(babel())
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(gulpIf(!isDebug, uglify()))
+    .pipe(gulp.dest('dist'));
 });
 
-gulp.task('server', () => (
-	bs.init({
-		watchOptions: {
-			ignored: 'src/**/*'
-		},
-		files: ['dist/**/*'],
-		open: !!OPEN,
-		reloadOnRestart: true,
-		port: PORT || 3000,
-		snippetOptions: {
-			rule: {
-				match: /<\/body>/i
-			}
-		},
-		server: {
-			baseDir: [
-				'src',
-				'dist'
-			],
-			directory: false
-		},
-		tunnel: !!TUNNEL
-	})
-));
+gulp.task('server', () =>
+  bs.init({
+    watchOptions: {
+      ignored: 'src/**/*'
+    },
+    files: ['dist/**/*'],
+    open: !!OPEN,
+    reloadOnRestart: true,
+    port: PORT || 3000,
+    snippetOptions: {
+      rule: {
+        match: /<\/body>/i
+      }
+    },
+    server: {
+      baseDir: ['src', 'dist'],
+      directory: false
+    },
+    tunnel: !!TUNNEL
+  })
+);
 
 gulp.task('copy', () => {
-	gulp.src(['src/**/*.*', '!src/*.js'])
-		.pipe(gulp.dest('dist'));
+  gulp.src(['src/**/*.*', '!src/*.js']).pipe(gulp.dest('dist'));
 });
 
-gulp.task('watch', () => { 
-	gulp.watch('src/*.html', () => runSequence('copy'));
-	gulp.watch('src/*.js', () => runSequence('scripts'));
+gulp.task('watch', () => {
+  gulp.watch('src/*.html', () => runSequence('copy'));
+  gulp.watch('src/*.js', () => runSequence('scripts'));
 });
 
 gulp.task('build', () => runSequence('styles', 'scripts'));
--- src/tooltip.js
+++ src/tooltip.js
@@ -1,128 +1,128 @@
 ;(function ( global ) {
-	
-	/**
-	 * The polyfill for Element.closest() method
-	 */
-	(function(el) {
-		el.matches = el.matches || el.mozMatchesSelector || el.msMatchesSelector || el.oMatchesSelector || el.webkitMatchesSelector;
-		el.closest = el.closest || function closest(selector) {
-			if (!this) return null;
-				if (this.matches(selector)) return this;
-				if (!this.parentElement) {return null}
-				else return this.parentElement.closest(selector)
-		};
-	}(Element.prototype));
+  
+  /**
+   * The polyfill for Element.closest() method
+   */
+  (function(el) {
+    el.matches = el.matches || el.mozMatchesSelector || el.msMatchesSelector || el.oMatchesSelector || el.webkitMatchesSelector;
+    el.closest = el.closest || function closest(selector) {
+      if (!this) return null;
+        if (this.matches(selector)) return this;
+        if (!this.parentElement) {return null}
+        else return this.parentElement.closest(selector)
+    };
+  }(Element.prototype));
 
-	global.Tooltip = (function () {
+  global.Tooltip = (function () {
 
-		const tooltipOptions = {
-			whereToShow: 'bottom',
-			duration: '.3s',
-			property: 'opacity',
-			tooltip: '*[title]'
-		};
-		let tooltipInstance = null;
-		
-		function init() {
-			const head = document.getElementsByTagName('head')[0];
-			const body = document.getElementsByTagName('body')[0];
-			let tooltip;
-			let styles;
+    const tooltipOptions = {
+      whereToShow: 'bottom',
+      duration: '.3s',
+      property: 'opacity',
+      tooltip: '*[title]'
+    };
+    let tooltipInstance = null;
+    
+    function init() {
+      const head = document.getElementsByTagName('head')[0];
+      const body = document.getElementsByTagName('body')[0];
+      let tooltip;
+      let styles;
 
-			function build() {
-				tooltip = document.createElement('div');
-				tooltip.className = 'tooltip';
-				styles = document.createElement('style');
-				styles.innerHTML = '.tooltip{position:fixed;top:-9999px;left:0;padding:4px;max-width: 150px;background:#fff;box-shadow:0px 2px 4px rgba(0,0,0,.3);opacity:0;transition-property:opacity;}';
-				mount();
-			}
+      function build() {
+        tooltip = document.createElement('div');
+        tooltip.className = 'tooltip';
+        styles = document.createElement('style');
+        styles.innerHTML = '.tooltip{position:fixed;top:-9999px;left:0;padding:4px;max-width: 150px;background:#fff;box-shadow:0px 2px 4px rgba(0,0,0,.3);opacity:0;transition-property:opacity;}';
+        mount();
+      }
 
-			function setStyles() {
-				tooltip.style.transitionDuration = tooltipOptions.duration;
-				tooltip.style.transitionProperty = tooltipOptions.property;
-			}
+      function setStyles() {
+        tooltip.style.transitionDuration = tooltipOptions.duration;
+        tooltip.style.transitionProperty = tooltipOptions.property;
+      }
 
-			function mount() {
-				head.appendChild(styles);
-				body.appendChild(tooltip);
-				setStyles();
-			}
+      function mount() {
+        head.appendChild(styles);
+        body.appendChild(tooltip);
+        setStyles();
+      }
 
-			function unmount() {
-				head.removeChild(styles);
-				body.removeChild(tooltip);
-			}
+      function unmount() {
+        head.removeChild(styles);
+        body.removeChild(tooltip);
+      }
 
-			function showTooltip(event) {
-				if (event.target.closest(tooltipOptions.tooltip)) {
-					const elToTooltip = event.target.closest(tooltipOptions.tooltip);
-					const wWidth = window.innerWidth;
-					const rect = elToTooltip.getBoundingClientRect();
-					const pos = rect.right + elToTooltip.scrollWidth > wWidth ? 'right' : 'left';
-					tooltip.textContent = elToTooltip.title;
-					switch (tooltipOptions.whereToShow) {
-						case 'bottom': {
-							tooltip.style.top = `${rect.top + rect.height + 4}px`;
-							break;
-						}
-						case 'top': {
-							tooltip.style.top = `${rect.top - tooltip.scrollHeight - 4}px`;
-							break
-						}
-					}
-					tooltip.style.opacity = 1;
-					switch (pos) {
-						case 'left':
-							tooltip.style.left = `${rect.left}px`;
-							break;
-						case 'right':
-							tooltip.style.left = `${rect.right - tooltip.scrollWidth}px`;
-							break;
-					}
-				}
-			}
+      function showTooltip(event) {
+        if (event.target.closest(tooltipOptions.tooltip)) {
+          const elToTooltip = event.target.closest(tooltipOptions.tooltip);
+          const wWidth = window.innerWidth;
+          const rect = elToTooltip.getBoundingClientRect();
+          const pos = rect.right + elToTooltip.scrollWidth > wWidth ? 'right' : 'left';
+          tooltip.textContent = elToTooltip.title;
+          switch (tooltipOptions.whereToShow) {
+            case 'bottom': {
+              tooltip.style.top = `${rect.top + rect.height + 4}px`;
+              break;
+            }
+            case 'top': {
+              tooltip.style.top = `${rect.top - tooltip.scrollHeight - 4}px`;
+              break
+            }
+          }
+          tooltip.style.opacity = 1;
+          switch (pos) {
+            case 'left':
+              tooltip.style.left = `${rect.left}px`;
+              break;
+            case 'right':
+              tooltip.style.left = `${rect.right - tooltip.scrollWidth}px`;
+              break;
+          }
+        }
+      }
 
-			function hideTooltip(event) {
-				if (event.target.closest(tooltipOptions.tooltip)) {
-					tooltip.style.top = '-9999px';
-					tooltip.style.opacity = '0';
-				}
-			}
+      function hideTooltip(event) {
+        if (event.target.closest(tooltipOptions.tooltip)) {
+          tooltip.style.top = '-9999px';
+          tooltip.style.opacity = '0';
+        }
+      }
 
-			function setOptions(options) {
-				for (const key in options) {
-					tooltipOptions[key] = options[key];
-				}
-				setStyles();
-			}
+      function setOptions(options) {
+        for (const key in options) {
+          tooltipOptions[key] = options[key];
+        }
+        setStyles();
+      }
 
-			build();
-			body.addEventListener('mouseover', showTooltip);
-			body.addEventListener('mouseout', hideTooltip);
+      build();
+      body.addEventListener('mouseover', showTooltip);
+      body.addEventListener('mouseout', hideTooltip);
 
-			return {
-				reInit: () => {
-					if (!tooltipInstance) {
-						tooltipInstance = init();
-					}
-					return tooltipInstance;
-				},
-				setOptions: setOptions,
-				destroy: () => {
-					body.removeEventListener('mouseover', showTooltip);
-					body.removeEventListener('mouseout', hideTooltip);
-					unmount();
-					return tooltipInstance = null;
-				}
-			};
-	
-		};
-	
-		if (!tooltipInstance) {
-			tooltipInstance = init();
-		}
-		return tooltipInstance;
-	
-	}());
+      return {
+        reInit: () => {
+          if (!tooltipInstance) {
+            tooltipInstance = init();
+          }
+          return tooltipInstance;
+        },
+        setOptions: setOptions,
+        destroy: () => {
+          body.removeEventListener('mouseover', showTooltip);
+          body.removeEventListener('mouseout', hideTooltip);
+          unmount();
+          return tooltipInstance = null;
+        }
+      };
+  
+    };
+  
+    if (!tooltipInstance) {
+      tooltipInstance = init();
+    }
+    return tooltipInstance;
+  
+  }());
 
 }( window ));
